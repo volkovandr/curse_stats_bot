4fbb4018467181e629f364645663dafafee07a2b
volkovandr <16303028+volkovandr@users.noreply.github.com>
2023-07-02 21:55:40 +0200

    Bot can read messages and say goodbye to all the chats he was talked to

--- src/main/scala/volkovandr/cursestatsbot/Entrypoint.scala
+++ src/main/scala/volkovandr/cursestatsbot/Entrypoint.scala
@@ -2,16 +2,26 @@ package volkovandr.cursestatsbot
 
 import org.telegram.telegrambots.meta.TelegramBotsApi
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException
+import org.telegram.telegrambots.meta.generics.BotOptions
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession
 
 object Entrypoint extends App {
   println("Starting...")
   try {
     val botsApi = new TelegramBotsApi(classOf[DefaultBotSession])
-    botsApi.registerBot(new bot.Bot(args(0)))
+    val b = new bot.Bot(args(0))
+    val botSession = botsApi.registerBot(b)
     println("Started!")
+
+    sys.addShutdownHook {
+      println("Saying goodbye...")
+      b.sayGoodbye()
+      println("Stopping...")
+      botSession.stop()
+      println("Stopped!")
+    }
   } catch {
     case tex: TelegramApiException => tex.printStackTrace()
-    case e => throw e
+    case e: Throwable => throw e
   }
 }
--- src/main/scala/volkovandr/cursestatsbot/bot/Bot.scala
+++ src/main/scala/volkovandr/cursestatsbot/bot/Bot.scala
@@ -1,21 +1,48 @@
 package volkovandr.cursestatsbot.bot
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot
-import org.telegram.telegrambots.meta.api.objects.Update
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod
+import org.telegram.telegrambots.meta.api.methods.send.{SendDocument, SendMessage}
+import org.telegram.telegrambots.meta.api.objects.{Message, Update}
+
+import java.time.Instant
+import scala.collection.mutable
 
 class Bot(apiKey: String) extends TelegramLongPollingBot(apiKey) {
+
+  private val chats: mutable.Set[Long] = mutable.Set()
   override def onUpdateReceived(update: Update): Unit = {
     if(update.hasMessage && update.getMessage.hasText) {
       println("Message received!")
       println(s"UpdateID: ${update.getUpdateId}")
-      println(s"Date: ${update.getMessage.getDate}")
-      println(s"FromID: ${update.getMessage.getForwardFromChat}")
+      println(s"Date: ${Instant.ofEpochSecond(update.getMessage.getDate.longValue())}")
+      println(s"FromID: ${update.getMessage.getFrom.getId}")
       println(s"ChatID: ${update.getMessage.getChatId}")
       println(s"Username: ${update.getMessage.getFrom.getUserName}")
+      println(s"Name: ${update.getMessage.getFrom.getFirstName} ${update.getMessage.getFrom.getLastName}")
       println(s"Message: ${update.getMessage.getText}")
       println("-----------------------------------")
+      chats.add(update.getMessage.getChatId)
     }
   }
 
   override def getBotUsername: String = "CurseStatsBot"
+
+  def sayGoodbye(): Unit = {
+    chats
+      .map(_.longValue())
+      .foreach {
+      chatId =>
+        val goodbyeMessage = new SendMessage()
+        goodbyeMessage.setChatId(chatId)
+        goodbyeMessage.setText("Goodbye!")
+
+        try {
+          execute[Message, SendMessage](goodbyeMessage)
+        }
+        catch {
+          case e: Throwable => e.printStackTrace()
+        }
+    }
+  }
 }
